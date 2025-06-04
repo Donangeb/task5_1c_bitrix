1208df5bbbca5d0c35d5a33bf9d9faa5f6581710
Donangeb <115127616+Donangeb@users.noreply.github.com>
2025-06-04 22:32:55 +0300

    Update Iblock.php

--- local/modules/dev.site/lib/Agents/Iblock.php
+++ local/modules/dev.site/lib/Agents/Iblock.php
@@ -7,53 +7,117 @@ class Iblock
 {
     public static function clearOldLogs()
     {
-        
-        
-        if (\Bitrix\Main\Loader::includeModule('iblock')) {
-        $iblockId = \Only\Site\Helpers\IBlock::getIblockID('QUARRIES_SEARCH', 'SYSTEM');
-        
-        // Получаем общее количество элементов
-        $rsCount = \CIBlockElement::GetList([], [
-            'IBLOCK_ID' => $iblockId
-        ], [], false, ['ID']);
-        $totalCount = $rsCount->SelectedRowsCount();
-        
-        // Если элементов больше 10, удаляем старые
-        if ($totalCount > 10) {
-            // Получаем ID 10 самых новых элементов
-            $rsNewest = \CIBlockElement::GetList(
-                ['TIMESTAMP_X' => 'DESC'],
-                ['IBLOCK_ID' => $iblockId],
-                false,
-                ['nTopCount' => 10],
-                ['ID']
-            );
-            
-            $excludeIds = [];
-            while ($arItem = $rsNewest->Fetch()) {
-                $excludeIds[] = $arItem['ID'];
-            }
-            
-            // Удаляем все элементы, кроме 10 самых новых
-            $rsToDelete = \CIBlockElement::GetList(
-                ['TIMESTAMP_X' => 'ASC'],
-                [
-                    'IBLOCK_ID' => $iblockId,
-                    '!ID' => $excludeIds
-                ],
-                false,
-                false,
-                ['ID']
-            );
-            
-            while ($arItem = $rsToDelete->Fetch()) {
-                \CIBlockElement::Delete($arItem['ID']);
+        // Проверяем, что модуль iblock загружен
+        if (!\Bitrix\Main\Loader::includeModule('iblock')) {
+            return '\\' . __CLASS__ . '::' . __FUNCTION__ . '();';
+        }
+
+        try {
+            // Получаем ID инфоблока
+            $iblockId = \Only\Site\Helpers\IBlock::getIblockID('LOG', 'services');
+        } catch (\Exception $e) {
+            // Логируем ошибку, если инфоблок не найден
+            \CEventLog::Add([
+                "SEVERITY" => "ERROR",
+                "AUDIT_TYPE_ID" => "IBLOCK_ERROR",
+                "MODULE_ID" => "iblock",
+                "DESCRIPTION" => "Не удалось получить ID инфоблока: " . $e->getMessage(),
+            ]);
+            return '\\' . __CLASS__ . '::' . __FUNCTION__ . '();';
+        }
+
+        // Получаем общее количество элементов (надежный способ)
+        $totalCount = 0;
+        $rsElements = \CIBlockElement::GetList(
+            [],
+            ['IBLOCK_ID' => $iblockId],
+            false,
+            false,
+            ['ID']
+        );
+
+        if (!is_object($rsElements)) {
+            \CEventLog::Add([
+                "SEVERITY" => "ERROR",
+                "AUDIT_TYPE_ID" => "IBLOCK_ERROR",
+                "MODULE_ID" => "iblock",
+                "DESCRIPTION" => "GetList вернул не объект",
+            ]);
+            return '\\' . __CLASS__ . '::' . __FUNCTION__ . '();';
+        }
+
+        while ($rsElements->Fetch()) {
+            $totalCount++;
+        }
+
+        // Если элементов 10 или меньше - ничего не делаем
+        if ($totalCount <= 10) {
+            return '\\' . __CLASS__ . '::' . __FUNCTION__ . '();';
+        }
+
+        // Получаем ID 10 самых новых элементов
+        $rsNewest = \CIBlockElement::GetList(
+            ['TIMESTAMP_X' => 'DESC'],
+            ['IBLOCK_ID' => $iblockId],
+            false,
+            ['nTopCount' => 10],
+            ['ID']
+        );
+
+        if (!is_object($rsNewest)) {
+            \CEventLog::Add([
+                "SEVERITY" => "ERROR",
+                "AUDIT_TYPE_ID" => "IBLOCK_ERROR",
+                "MODULE_ID" => "iblock",
+                "DESCRIPTION" => "GetList для новых элементов вернул не объект",
+            ]);
+            return '\\' . __CLASS__ . '::' . __FUNCTION__ . '();';
+        }
+
+        $excludeIds = [];
+        while ($arItem = $rsNewest->Fetch()) {
+            $excludeIds[] = $arItem['ID'];
+        }
+
+        // Удаляем все элементы, кроме 10 самых новых
+        $rsToDelete = \CIBlockElement::GetList(
+            ['TIMESTAMP_X' => 'ASC'],
+            [
+                'IBLOCK_ID' => $iblockId,
+                '!ID' => $excludeIds
+            ],
+            false,
+            false,
+            ['ID']
+        );
+
+        if (!is_object($rsToDelete)) {
+            \CEventLog::Add([
+                "SEVERITY" => "ERROR",
+                "AUDIT_TYPE_ID" => "IBLOCK_ERROR",
+                "MODULE_ID" => "iblock",
+                "DESCRIPTION" => "GetList для удаления вернул не объект",
+            ]);
+            return '\\' . __CLASS__ . '::' . __FUNCTION__ . '();';
+        }
+
+        $deletedCount = 0;
+        while ($arItem = $rsToDelete->Fetch()) {
+            if (\CIBlockElement::Delete($arItem['ID'])) {
+                $deletedCount++;
             }
         }
-    }
-    return '\\' . __CLASS__ . '::' . __FUNCTION__ . '();';
-    }
 
+        // Логируем результат работы
+        \CEventLog::Add([
+            "SEVERITY" => "INFO",
+            "AUDIT_TYPE_ID" => "IBLOCK_CLEANUP",
+            "MODULE_ID" => "iblock",
+            "DESCRIPTION" => "Удалено старых элементов: $deletedCount",
+        ]);
+
+        return '\\' . __CLASS__ . '::' . __FUNCTION__ . '();';
+    }
     public static function example()
     {
         global $DB;
